e93b2158971627703bfa393b2273907c6a0211ac
Rek <57406598+rekkun1-xkx-me@users.noreply.github.com>
2020-05-07 19:02:34 +0700

    Update anti-discord.js

--- anti-discord.js
+++ anti-discord.js
@@ -17,7 +17,7 @@
 (function() {
     'use strict';
     alert("Bỏ Discord một thời gian đi để mà học, học hành giờ như thế thì sau này lên coder thế lon nào được .___.")
-    var password = prompt("Password gồm 16 kí tự =))))", "Và tất nhiên mày sẽ không nhớ nó đâu =))))");
+    var password = prompt("Password gồm n kí tự =))))", "Và tất nhiên mày sẽ không nhớ nó đâu =))))");
             if (password == "5M8VXdAsN5hvhQkx2CusF7G8oopM8Yw3") {
                 alert("May mắn ghê nhỉ =))) Nhưng mày vẫn nên bỏ Discord");
                 window.location.replace('https://anti-discord.herokuapp.com/success.php/');
